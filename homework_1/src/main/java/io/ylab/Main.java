aa48dd77352c13ec25e6478af3c3ffe95ae7decd
Aleksandr Trofimov <shurikx6881@gmail.com>
2024-10-09 23:59:55 +0300

    teat: check menu functionality.

--- homework_1/src/main/java/io/ylab/Main.java
+++ homework_1/src/main/java/io/ylab/Main.java
@@ -1,10 +1,25 @@
 package io.ylab;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.ylab.app.menu.CommandMenu;
+import io.ylab.app.menu.CompositeMenu;
+import io.ylab.app.menu.MenuComponent;
 
 public class Main {
     public static void main(String[] args) {
 
+        MenuComponent mainMenu = new CompositeMenu("Гланое меню");
+
+        MenuComponent composite1 = new CompositeMenu("Composite 1");
+        MenuComponent composite2 = new CompositeMenu("Composite 2");
+
+        composite1.addMenu(new CommandMenu("Command menu 1_1"));
+        composite1.addMenu(new CommandMenu("Command menu 1_2"));
+        composite1.setParent(mainMenu);
+        composite2.setParent(mainMenu);
+
+        mainMenu.addMenu(composite1);
+        mainMenu.addMenu(composite2);
+
+        mainMenu.execute();
     }
 }
